e2b175081d7d953c348998caa5138ae1e0315a0f
Belhill100 <ecollazosbravo@gmail.com>
2021-11-26 11:51:31 -0500

    Cambios a Reclutdor

--- src/Model/Reclutador.cpp
+++ src/Model/Reclutador.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "Reclutador.h"
+
+void Reclutador::crearCandidato() {
+    Candidato * x = new Candidato("Juan", "jpcespedes", "jpcespedes", "JPCM", 1, "masculino");
+    this->candidatos.insert({1, x});
+
+    Candidato * x1 = new Candidato("Emma", "emmacoll", "emmacoll", "EMMA", 2, "masculino");
+    this->candidatos.insert({2, x1});
+}
--- src/Model/Reclutador.h
+++ src/Model/Reclutador.h
@@ -5,9 +5,22 @@
 #ifndef PRUEBA_RECLUTADOR_H
 #define PRUEBA_RECLUTADOR_H
 
+#include "Candidato.h"
+#include "Nacionalidad.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <map>
 
 class Reclutador {
-
+private:
+    map<int, Candidato*> candidatos;
+public:
+    void crearCandidato();
+    void agendarEntrevistas(int pasaporte);
+    void generarGuia(Candidato candidato);
+    void generarCarta(Candidato candidato);
 };
 
 
