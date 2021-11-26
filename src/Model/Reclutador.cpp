43300aeb8cc7c5eab47798acb8d4ae1c899941fb
Belhill100 <ecollazosbravo@gmail.com>
2021-11-26 12:35:22 -0500

    Se cambio el MAIN

--- src/Model/Reclutador.cpp
+++ src/Model/Reclutador.cpp
@@ -5,9 +5,9 @@
 #include "Reclutador.h"
 
 void Reclutador::crearCandidato() {
-    Candidato * x = new Candidato("Juan", "jpcespedes", "jpcespedes", "JPCM", 1, "masculino");
+    Candidato * x = new Candidato("Juan", "jpcespedes", "jpcespedes", "JPCM", 1, false);
     this->candidatos.insert({1, x});
 
-    Candidato * x1 = new Candidato("Emma", "emmacoll", "emmacoll", "EMMA", 2, "masculino");
+    Candidato * x1 = new Candidato("Emma", "emmacoll", "emmacoll", "EMMA", 2, false);
     this->candidatos.insert({2, x1});
 }
