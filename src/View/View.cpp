e0948a86b344fe1ece40bf0cc3c4764e4b9417e7
Belhill100 <ecollazosbravo@gmail.com>
2021-11-26 12:37:12 -0500

    Se añade el View.h y el View.cpp

---
+++ src/View/View.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ecoll on 26/11/2021.
+//
+
+#include "View.h"
---
+++ src/View/View.h
@@ -0,0 +1,14 @@
+//
+// Created by ecoll on 26/11/2021.
+//
+
+#ifndef RECRUITMENT_JPC_EC_DG_VIEW_H
+#define RECRUITMENT_JPC_EC_DG_VIEW_H
+
+
+class View {
+
+};
+
+
+#endif //RECRUITMENT_JPC_EC_DG_VIEW_H
