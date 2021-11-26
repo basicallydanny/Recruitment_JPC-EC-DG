56a8760e1d629acece15e2e833480d5f11ea2d37
Belhill100 <ecollazosbravo@gmail.com>
2021-11-26 14:26:47 -0500

    Diseño del view.h y view.cpp, correccion del Reclutador

--- src/Model/Reclutador.cpp
+++ src/Model/Reclutador.cpp
@@ -1,7 +1,3 @@
-//
-// Created by ecoll on 24/11/2021.
-//
-
 #include "Reclutador.h"
 
 void Reclutador::crearCandidato() {
--- src/View/View.cpp
+++ src/View/View.cpp
@@ -1,5 +1,31 @@
-//
-// Created by ecoll on 26/11/2021.
-//
-
 #include "View.h"
+
+View::View(){
+}
+
+void View::mostrarMenu(){
+    int opcion;
+    do{
+        cout << "Menu Reclutador\n";
+        cout << "1. Consultar los conocimientos culturales\n";
+        cout << "2. Crear candidatos\n";
+        cout << "3. Programar entrevistas\n";
+        cout << "4. Generar guía de entrevistas\n";
+        cout << "5. Generar la carta de bienvenida\n";
+        cout << "0. Salir \n";
+        std::cout << "Digita el numero: ";
+        std::cin >> opcion;
+        switch (opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    } while (opcion != 0);
+}
