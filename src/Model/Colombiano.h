9941a533baefbaea31e4a5f465ffaa366741fa3c
Belhill100 <ecollazosbravo@gmail.com>
2021-11-27 20:01:03 -0500

    Polimorfismo en consultar conocimientos

--- src/Model/Colombiano.h
+++ src/Model/Colombiano.h
@@ -8,16 +8,16 @@ using namespace std;
 
 class Colombiano : public Nacionalidad {
 protected:
-    string nombrePais;
-    bool tipoCultura;
-    string contactoVisual;
-    string gestos;
-    string tacto;
-    string respeto;
-    string gestionTiempo;
-    string estiloComunicacion;
-    bool tieneSiesta;
-    string festividades;
+    string nombrePais = "Colombia";
+    string tipoCultura = "expresiva";
+    string contactoVisual = "No tienen problema frente al contacto visual prolongado.";
+    string gestos = "la expresión 'paila' para decir que se está mal y el señalar con la punta de los labios";
+    string tacto = "suelen ser muy abiertos al contacto, sin importar de quién lo reciben.";
+    string respeto = " ";
+    string gestionTiempo = "se obtienen resultados, aunque los metodos de gestion pueden ser ortodoxos";
+    string estiloComunicacion = "tienen una gran velocidad y expresiones propias que emplean muchas comparaciones para darse a entender.";
+    bool tieneSiesta = false;
+    string festividades = "Carnaval de Blancos y Negros, Carnaval de Barranquilla, Feria de Cali";
 
 public:
     virtual ~Colombiano();
--- src/Model/Coreano.cpp
+++ src/Model/Coreano.cpp
@@ -1,5 +1,18 @@
-//
-// Created by ecoll on 24/11/2021.
-//
-
 #include "Coreano.h"
+
+void consultarConocimientos(){
+    cout << nombrePais << endl;
+    cout << "Su cultura es " << tipoCultura << "\n"
+         << contactoVisual << "\n"
+         << "Sus gestos mas notables son " << gestos << "\n"
+         << "En cuanto al tacto, " << tacto << "\n"
+         << "Su perspectiva frente al respeto es " << respeto << "\n"
+         << "Gestionan el tiempo de tal manera que " << gestionTiempo << "\n"
+         << "Para hablar " << estiloComunicacion << endl;
+    if (tieneSiesta == True ) {
+        write << "Su cultura tiene siestas." << "\n";
+    } else {
+        write << "Su cultura no tiene siestas." << "\n";
+    }
+    cout << "Algunas festividades son: " << festividades << "\n";
+}
