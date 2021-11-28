d37369a9e4a0a7f131c50b06e5506c8924d49a22
Juan Paulo Cespedes <jpcespedeshotmail.com>
2021-11-27 20:41:56 -0500

    Se pone la informacion de Colombia Se pone la información de Corea Se pone la información de Francia Se pone la información de Sudafrica

--- src/Model/Frances.h
+++ src/Model/Frances.h
@@ -5,16 +5,16 @@
 
 class Frances : public Nacionalidad {
 protected:
-    string nombrePais;
-    bool tipoCultura;
-    string contactoVisual;
-    string gestos;
-    string tacto;
-    string respeto;
-    string gestionTiempo;
-    string estiloComunicacion;
-    bool tieneSiesta;
-    string festividades;
+    string nombrePais = "Francia";
+    string tipoCultura = "heredar la reputación de ser un país fundador en terminos de protocolo y buenos modales.";
+    string contactoVisual = "El contacto visual es perfecto para lograr una primera impresion positiva.";
+    string gestos = "cuando se colocan la punta de los cinco dedos juntos y hacia arriba, le da un acento al discurso y significa que lo que se dice es importante. La mano extendida dirigiendo la palma hacia e interlocutor, y haciendo movimientos ligeros de derecha a izquierda y viceversa, quiere decir que rechazas de manera cortes lo que el interlocutor propone.";
+    string tacto = "es costumbre saludarse con un apreton de manos firme, pero no en exceso. El saludo habitual entre amigos o familiares es el beso en las mejillas, entre mujeres, o entre hombres y mujeres.";
+    string respeto = "liberal, motivando a sus receptores a expresarse con total libertad.";
+    string gestionTiempo = "comparten una fuerte segmentación del trabajo, por lo cual tienden a recurrir a sus adyacentes para optimizar el resultado final.";
+    string estiloComunicacion = "utilizan muchas expresiones figuradas que pueden resultar desconcertantes. Pide que te las expliquen y disfrutara utilizandolas despues.";
+    bool tieneSiesta = true;
+    string festividades = "La fiesta de las crepas, la Galette des Rois, Mercados Navidenos.";
 
 public:
     virtual ~Frances();
--- src/Model/Sudafricano.h
+++ src/Model/Sudafricano.h
@@ -5,16 +5,16 @@
 
 class Sudafricano : public Nacionalidad {
 protected:
-    string nombrePais;
-    bool tipoCultura;
-    string contactoVisual;
-    string gestos;
-    string tacto;
-    string respeto;
-    string gestionTiempo;
-    string estiloComunicacion;
-    bool tieneSiesta;
-    string festividades;
+    string nombrePais = "Sudafrica";
+    string tipoCultura = "se destaca por su diversidad étnica y cultural. Por lo tanto, no existe una unica cultura de Sudafrica. Preguntale sobre su propia experiencia.";
+    string contactoVisual = "El contacto visual directo es frecuente, aunque mantener la mirada fija por largo tiempo podría ser considerado retador.";
+    string gestos = "Se saluda con un apreton de manos, con una sonrisa y mirando a los ojos, y se saluda primero a la persona de mayor jerarquia.";
+    string tacto = "el espacio personal es reducido. Al conversar es común que las personas se encuentren físicamente cerca, en ocasiones hombro con hombro.";
+    string respeto = "El espacio se respeta dependiendo de quien signifique esa persona para ti. No es lo mismo saludar a tu padre que a tu amigo.";
+    string gestionTiempo = "Respetan mucho el tiempo que tiene cada cosa en su dia a dia.";
+    string estiloComunicacion = "Su estilo de comunicacion depende muchas veces de su region, por lo que esto hace que varien las lenguas habladas.";
+    bool tieneSiesta = true;
+    string festividades = "Fin del apartheid, festivales de rock y jazz, dia de la diversidad.";
 
 public:
     virtual ~Sudafricano();
